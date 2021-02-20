a1e13b7f7297346fe904ba390278fb0a130cb2ae
sami10644 <abdullahassami@gmail.com>
2021-02-21 04:57:12 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def next_palindrome(n):
+    n=n+1
+    while not is_palindrome(n):
+        n +=1
+    return n
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+if __name__=="__main__":
+    n=int(input("how many want u:\n"))
+    numbers=[]
+
+for i in range(n):
+    number=int(input("entr the no:\n"))
+    numbers.append(number)
+
+for i in range(n):
+    print(
+          f"nxt palindrome for {numbers[i]}is {next_palindrome(numbers[i])}")
